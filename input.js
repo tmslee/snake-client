2050277836c56ebfd0b0085ad683ac61c1523a3b
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-13 04:31:05 +0000

    added sending movement commands to server from handleUserInput function in input.js

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,6 +12,22 @@ const setupInput = function() {
 
 const handleUserInput = function (key) {
   if (key === '\u0003') process.exit();
+  else {
+    switch(key){
+      case 'w':
+        connection.write('Move: up');
+        break;
+      case 'a':
+        connection.write('Move: left');
+        break;
+      case 's':
+        connection.write('Move: down');
+        break;
+      case 'd':
+        connection.write('Move: right');
+        break;
+    }
+  }
 };
 
 module.exports = {setupInput};
