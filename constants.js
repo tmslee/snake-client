2de287294265cfb9058fe5326061b8ceeb55e9eb
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-13 17:20:50 +0000

    moveed constants to a separate file

---
+++ constants.js
@@ -0,0 +1,14 @@
+const IP = '10.0.2.15'
+const PORT = 50541
+const INPUT_MAP = {
+  'w':'Move: up',
+  'a':'Move: left',
+  's':'Move: down',
+  'd':'Move: right',
+  'e':'Say: ssssssssssssss',
+  'r':'Say: SSSSSSSSSSsssss',
+  't':'Say: SSSsssSSS',
+  'y':'Say: sssssSSSSSSSSS'
+};
+
+module.exports = {IP, PORT, INPUT_MAP};
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 let connection;
+const {INPUT_MAP} = require('./constants');
 
 const setupInput = function(conn) {
   connection = conn;
@@ -13,33 +14,7 @@ const setupInput = function(conn) {
 const handleUserInput = function (key) {
   if (key === '\u0003') process.exit();
   else {
-    switch(key){
-      case 'w':
-        connection.write('Move: up');
-        break;
-      case 'a':
-        connection.write('Move: left');
-        break;
-      case 's':
-        connection.write('Move: down');
-        break;
-      case 'd':
-        connection.write('Move: right');
-        break;
-      case 'e':
-        connection.write('Say: ssssssssssssss');
-        break;
-      case 'r':
-        connection.write('Say: SSSSSSSSSSsssss');
-        break;
-      case 't':
-        connection.write('Say: SSSsssSSS');
-        break;
-      case 'y':
-        connection.write('Say: sssssSSSSSSSSS');
-        break;
-      
-    }
+    connection.write(INPUT_MAP[key]);
   }
 };
 
