f51ece0b0407e9dc8a89b4a673b42ca01950d1ef
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-13 03:04:28 +0000

    made client.js, moved connect function to there and exported it

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+const connect = function(){
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('data', data => {
+    console.log(`server message: ${data}`);
+  });
+
+  return conn;
+}
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,18 +1,4 @@
-const net = require('net');
-
-const connect = function(){
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-
-  conn.on('data', data => {
-    console.log(`server message: ${data}`);
-  });
-
-  return conn;
-}
+const {connect} = require('./client');
 
 console.log('Connecting...');
 connect();
